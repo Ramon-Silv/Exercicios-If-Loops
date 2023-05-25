e04befecc7498c11e6033295d5cbafd9afa59ff6
Ramon-Silv <silvaramonaugusto@gmail.com>
2023-05-25 01:54:29 -0300

    faltam alguns exercícios

---
+++ Exercicios-If-Else/js/exer17.js
@@ -0,0 +1,9 @@
+let numero = parseInt(prompt("Informe um número menor que 1000:"))
+
+let centena = Math.floor(numero / 100);
+let dezena = Math.floor((numero % 100) / 10);
+let unidade = Math.floor(numero % 10);
+
+extenso
+
+document.write("O número "+numero+" possui")
